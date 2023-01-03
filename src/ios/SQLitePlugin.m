eed12426caa1f38add90d2af0ad557a9fcd7488f
BhandariBhumin <bhumin.bhandari@pfizer.com>
2023-01-03 13:07:21 +0530

    Update SQLitePlugin.m

--- src/ios/SQLitePlugin.m
+++ src/ios/SQLitePlugin.m
@@ -306,6 +306,31 @@
     }
 }
 
+
+/*
+ * Arguments:
+ *
+ *   unused - Ignored in this case, see the documentation for sqlite3_exec
+ *    count - The number of columns in the result set
+ *     data - The row's data
+ *  columns - The column names
+ */
+static int my_special_callback(void *unused, int count, char **data, char **columns)
+{
+    int idx;
+
+    printf("There are %d column(s)\n", count);
+  
+    for (idx = 0; idx < count; idx++) {
+        printf("The data in column \"%s\" is: %s\n", columns[idx], data[idx]);
+    }
+
+    printf("\n");
+   
+    return 0;
+}
+
+
 -(void) logSqlError: (sqlite3*)db message:(NSString*) message
 {
     const char *errmsg = sqlite3_errmsg(db);
